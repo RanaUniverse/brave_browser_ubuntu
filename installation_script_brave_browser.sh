62e3a65426b9cbd09e098c43329ad31b71a7318a
RanaUniverse <RanaUniverse321@gmail.com>
2025-08-13 13:00:02 +0530

    installation script got updated

--- installation_script_brave_browser.sh
+++ installation_script_brave_browser.sh
@@ -1,11 +1,13 @@
-version = 1.78.102
 
 echo "This will install the Brave Browser of Version: $version in this machine."
-
 
 echo "This will install the brave browser in this machine."
 
 # sudo dpkg -i *.deb
 sudo apt install ./brave-*.deb
+# It should install only brave & brave keyring applicaion.
 
-echo "To check brave browser version, run this: brave-browser"
+echo "To Open Brave Browser Application, brave-browser."
+
+echo "To check brave browser version, run this: brave-browser --version"
+
